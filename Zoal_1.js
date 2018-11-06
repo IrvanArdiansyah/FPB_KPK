f3fff63f22136e059fc524858e00d1f743c045e4
IrvanArdiansyah <irvardiansyah@gmail.com>
2018-11-06 10:02:43 +0700

    FPB dan KPK

--- Zoal_1.js
+++ Zoal_1.js
@@ -2,19 +2,24 @@ function fpb_kpk(x, y) {
     nampung = [];
     nampung1 = [];
     nampung2 = [];
+    nampung3 = []; 
+    nampung4 = [];
+    nampung5 = [];
+    nampung6 = [];
     nampungFpb = [];
+    nampungKpk = [];
         for(i=1; i<=x; i++){
             if(x%i==0){
             nampung.push(i)
             }
         }
-        console.log(nampung)
+        // console.log(nampung)
         for(i=0; i<=y; i++){
             if(y%i == 0) {
             nampung1.push(i)
             }
         }
-        console.log(nampung1)
+        // console.log(nampung1)
         for (i=0; i<=nampung.length; i++) {
             for (j=0; j<=nampung1.length; j++) {
                 if (nampung[i]/nampung1[j] == 1) 
@@ -23,7 +28,27 @@ function fpb_kpk(x, y) {
         }
         nampung2.reverse();
         nampungFpb.push(nampung2[0])
-        console.log(nampung2)
-        console.log(nampungFpb)
+        // console.log(nampung2)
+        // console.log(nampungFpb)
+        for (i=1; i<=y; i++) {
+            nampung3.push(x*i)
+            }
+        // console.log(nampung3)
+        for (i=1; i<=y; i++) {
+            nampung4.push(y*i)
+            }
+        // console.log(nampung4)
+        for (i=1; i<=nampung3.length; i++) {
+            for (j=1; j<=nampung4.length; j++) {
+                if (nampung3[i]/nampung4[j] == 1) 
+                nampung6.push(nampung3[i])
+            }
+        }
+    // console.log(nampung6)
+    nampungKpk.push(nampung6[0])
+    // console.log(nampungKpk)
+    console.log(`FPB ${x} & ${y} = ${nampungFpb}
+KPK ${x} & ${y} = ${nampungKpk}`)
     }
+    
     fpb_kpk(2,3)
